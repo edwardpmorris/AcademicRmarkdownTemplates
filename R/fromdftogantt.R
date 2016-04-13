0436f00aeab834d9d04e742ac633238dfa17a4cf
edwardpmorris <edward.morris@uca.es>
2016-04-13 23:50:11 +0200

    added gantt chart fuction

---
+++ R/fromdftogantt.R
@@ -0,0 +1,54 @@
+#' Make a Gantt diagram from a dataframe
+#'
+#' @param df Dataframe
+#' @param Title Title of  diagram
+#' @param filename Optionally write out to a html file. Default is not.
+#'
+#' @return A htmlwigets object representing a Gantt diagram
+#' @export
+#'
+#' @examples
+#' #' file_path <- "asana_export1.csv"
+#' project_start <- "2016-06-01"
+#' df <- parseAsana(file_path,project_start)
+#' df <- df[df$type %in% c(" Milestone", "Task"),]
+#' df[grep("Milestone", df$type),'type'] <- "crit"
+#' df[grep("Subtask", df$type),'type'] <- "done"
+#' df[grep("Task", df$type),'type'] <- "active"
+#' fromdftogantt(df)
+fromdftogantt<-function(df,Title="",filename=NULL){
+  txt<-paste("gantt","dateFormat  YYYY-MM-DD",paste("title",Title),"",sep="\n")
+  for(i in unique(df$section)){
+    txt<-paste(txt,paste("section",i),sep="\n")
+    for(j in which(df$section==i)){
+
+      txt<-paste(txt,paste0(df$task[j],":",df$type[j],",",
+                            df$start[j],",",
+                            df$end[j]),sep="\n")
+    }
+    txt<-paste0(txt,"\n")
+  }
+  m<-DiagrammeR::mermaid(txt)
+  m$x$config = list(ganttConfig = list(
+    axisFormatter = list(list(
+      "%m-%Y"
+      ,htmlwidgets::JS(
+        'function(d){ return d.getDate() == 1 }'
+      )
+    ))))
+  m$x$config = list(ganttConfig = list(
+    titleTopMargin=25,
+    barHeight=20,
+    barGap=5,
+    topPadding=50,
+    sidePadding=100,
+    gridLineStartPadding=35,
+    fontSize=11,
+    numberSectionStyles=2
+    ))
+  if(!is.null(filename)){
+    htmltools::save_html(htmltools::as.tags(m),file=filename)
+  } else{
+    return(m)
+  }
+}
---
+++ R/parseAsana.R
@@ -0,0 +1,86 @@
+#' Parse an Asana task list
+#'
+#' Export an Asana task list and convert it to a dataframe ready for making a
+#' Gantt diagram using DiagrammeR. As Asana does not have a 'Start Date' field
+#' Tags are used to indicate periods of time. For example 'start: 0' 'end: 6'
+#' represents a task that starts at month 0 and ends month 6. Tags are also
+#' used to indicate 'Milestone's (important events) and 'Deliverable's (outputs
+#' to be delivered by project). Sections are also selected...
+#'
+#' @param file_path File exported as CSV or JSON (not implemented) from Asana.
+#' @param project_start ISO 8601 date of project start.
+#'
+#' @return A dataframe suitable for producing a Gantt diagram.
+#' @export
+#'
+#' @examples
+#' file_path <- "asana_export1.csv"
+#' project_start <- "2016-06-01"
+#' df <- parseAsana(file_path,project_start)
+parseAsana <- function(file_path, project_start, format = "csv") {
+  switch (format,
+          csv = out <- read.csv(file_path, as.is = T),
+          JSON = return(print("Not implemented yet")))
+  # extract names, tags and due dates
+  tags <- out$Tags
+  dd <- out$Due.Date
+  tags <- lapply(tags, function(x) {
+    yaml::yaml.load(gsub(",", "\n", x))
+  })
+  nms <- out$Name
+  wp <- out$Projects
+  # convert tags
+  convertTags <- function(tag) {
+    df <- data.frame(start = "",
+                     end = "",
+                     type = "")
+    nms <- names(tag)
+    if (any(grepl("T", nms))) {
+      df$type <- "Task"
+    }
+    if (any(grepl("M", nms))) {
+      df$type <- paste(df$type, "Milestone")
+    }
+    if (any(grepl("D", nms))) {
+      df$type <- paste(df$type, "Deliverable")
+    }
+    if (any(grep("Start", nms))) {
+      df$start <- as.character(tag[['Start']])
+    }
+    if (any(grepl("End", nms))) {
+      df$end <- as.character(tag[['End']])
+    }
+    df$type <- sub(" +$", "", df$type)
+    if (df$type == "") {
+      df$type <- "Subtask"
+    }
+    return(df)
+  }
+  df <- do.call(rbind, lapply(tags, convertTags))
+  # convert to real dates
+  calcDate <- function(month, project_start) {
+    if (is.na(month)) {
+      out <- ""
+    } else{
+      out <-
+        as.character(format(as.POSIXct(project_start) + (month * 60 * 60 * 24 * 30), "%Y-%m-%d"))
+    }
+    return(out)
+  }
+  df$start <-
+    mapply(calcDate,
+           as.numeric(df$start),
+           MoreArgs = list(project_start = project_start))
+  df$end <-
+    mapply(calcDate,
+           as.numeric(df$end),
+           MoreArgs = list(project_start = project_start))
+  # add date to Milestones
+  df[grep("Milestone", df$type), 'end'] <- dd[grep("Milestone", df$type)]
+  df[grep("Milestone", df$type), 'start'] <- as.character(as.POSIXct(dd[grep("Milestone", df$type)]) - (1*60*60*24))
+  # prepare output df
+  df$pos <- paste0("P", rownames(df))
+  df$task <- gsub(":", "", out$Name)
+  df$section <- wp
+  return(df)
+}
