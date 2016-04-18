280aa2751a0674c3c065ca76fba645faff5ff782
edwardpmorris <edward.morris@uca.es>
2016-04-18 16:38:17 +0200

    update table

---
+++ R/addDocChangeTab.R
@@ -0,0 +1,25 @@
+yaml_path="document_change_record.yaml"
+#' Add a document change record table
+#'
+#' Convert a YAML document listing 'document changes' into a markdown table
+#'ready for inserting in a document.
+#'
+#' @param yaml_path Path to YAML file 
+#' @param table_out If option 'kable' given output is a markdown table, otherwise a dataframe
+#'
+#' @return If option 'kable' given output is a markdown table, otherwise a dataframe
+#' @export
+#'
+#' @examples
+#' yaml_path="document_change_record.yaml"
+#' addDocChangeTab(yaml_path, table_out="kable")
+addDocChangeTab <- function(yaml_path, table_out="kable"){
+  l <- yaml::yaml.load_file(yaml_path)
+  nms <- names(l[[1]])
+  df <- data.frame(t(matrix(unlist(l), nrow=5, byrow=F)),stringsAsFactors=FALSE)
+  names(df) <- nms
+  if (table_out=="kable"){
+    df <- knitr::kable(df)
+  } 
+  return(df)
+}
---
+++ inst/rmarkdown/templates/proposal/skeleton/README.md
@@ -0,0 +1,12 @@
+# Proposal template
+
+## Directory structure
+
++ `/data` Any data files; including export from Asana.
++ `/figures` Any images or graphics; including code definitions of diagrams (.mmd, .DOT)
++ `/config` Proposal metadata (`metadata.yaml`), bibliography (`bibliography.bib`), css and style tweaks.
+
+## Instructions
+
+Fill in `/config/metadata.yaml`, add BibTeX citations to `/config/bibliography.bib`, figures and data.
+
